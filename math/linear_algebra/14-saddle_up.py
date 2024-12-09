32b6115a393181ee3f1d8a867f2a276dfb9e5ad5
ravan <itzreone@gmail.com>
2024-12-09 19:31:58 +0400

    push

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 import numpy as np
 
-'''
-documentation
-documentation
-documentation
-'''
+
+"""
+This module contains a function to perform matrix multiplication using NumPy's
+matmul function. It takes two matrices (NumPy ndarrays) as input and returns
+their product.
+"""
+
 
 def np_matmul(mat1, mat2):
     """
-    mat1
-    mat2
+    Multiplies two matrices (mat1 and mat2) using NumPy's matmul function.
+    
+    Parameters:
+    - mat1: A NumPy ndarray representing the first matrix.
+    - mat2: A NumPy ndarray representing the second matrix.
+    
+    Returns:
+    - A new NumPy ndarray representing the result of matrix multiplication.
     """
     return np.matmul(mat1, mat2)
