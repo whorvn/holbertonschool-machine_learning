5f8a7f62992c162ae8dbe8b2303cbc0105b5f56a
ravan <itzreone@gmail.com>
2024-12-09 19:30:32 +0400

    doc

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
 
+'''
+documentation
+documentation
+documentation
+'''
 
 def np_matmul(mat1, mat2):
+    """
+    mat1
+    mat2
+    """
     return np.matmul(mat1, mat2)
