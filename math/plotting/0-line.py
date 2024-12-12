957b87e8af9a95b0e74b92a53a5956395b2d8fe6
ravan <itzreone@gmail.com>
2024-12-12 15:52:31 +0400

    doc

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -5,11 +5,9 @@ import matplotlib.pyplot as plt
 
 def line():
     """
-    Plots a cubic curve using matplotlib and numpy.
-
-    The function generates values from 0 to 10, raises them to the power of 3,
-    and then plots the resulting curve using matplotlib. The x-axis is limited
-    to the range 0-10, and the plot is displayed in red.
+    def(line)
+    numpy
+    matplotlib.pyplot
     """
     y = np.arange(0, 11) ** 3
     plt.xlim(0, 10)
