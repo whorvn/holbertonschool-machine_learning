55743f2e1a55da3956d644061f21bf0decd4e261
ravan <itzreone@gmail.com>
2024-12-12 15:53:15 +0400

    doc

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
+""" plots y as a line graph """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-    """
-    def(line)
-    numpy
-    matplotlib.pyplot
-    """
+
     y = np.arange(0, 11) ** 3
     plt.xlim(0, 10)
     plt.plot(y, color='red')
