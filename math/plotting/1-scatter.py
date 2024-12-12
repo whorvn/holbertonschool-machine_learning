20f714ce217c8b0397d7d118919e237862f32bf4
ravan <itzreone@gmail.com>
2024-12-12 15:59:55 +0400

    task 1

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""_summary_"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """_summary_"""
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    plt.title('Men\'s Height vs Weight')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.plot(x, y, 'm.')
+    plt.show()
