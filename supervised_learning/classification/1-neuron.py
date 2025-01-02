d1be866dd362c607a40827e970ba5e009e996208
ravan <itzreone@gmail.com>
2025-01-02 19:12:31 +0400

    private properties

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+documentation documentation
+documentation documentation
+documentation documentation
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    documentation documentation
+    documentation documentation
+    documentation documentation
+    """
+    def __init__(self, nx):
+        """
+        documentation documentation
+        documentation documentation
+        documentation documentation
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+        self.nx = nx
+
+    @property
+    def W(self):
+        """
+        gets the private instance attribute __W
+        __W is the weights vector for the neuron
+        """
+        return (self.__W)
+
+    @property
+    def b(self):
+        """
+        gets the private instance attribute __b
+        __b is the bias for the neuron
+        """
+        return (self.__b)
+
+    @property
+    def A(self):
+        """
+        gets the private instance attribute __A
+        __A is the activated output of the neuron
+        """
+        return (self.__A)
