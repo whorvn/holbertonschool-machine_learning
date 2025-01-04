badbcaf01760aaad523cac2b59b45fc8d6272572
ravan <itzreone@gmail.com>
2025-01-04 14:59:01 +0400

    task 5 gradient descent

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""
+documentation documentation
+documentation documentation
+documentation documentation
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    documentation documentation
+    documentation documentation
+    documentation documentation
+    """
+    def __init__(self, nx):
+        """
+        documentation documentation
+        documentation documentation
+        documentation documentation
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+        self.nx = nx
+
+    @property
+    def W(self):
+        """
+        gets the private instance attribute __W
+        __W is the weights vector for the neuron
+        """
+        return (self.__W)
+
+    @property
+    def b(self):
+        """
+        gets the private instance attribute __b
+        __b is the bias for the neuron
+        """
+        return (self.__b)
+
+    @property
+    def A(self):
+        """
+        gets the private instance attribute __A
+        __A is the activated output of the neuron
+        """
+        return (self.__A)
+
+    def forward_prop(self, X):
+        """
+        documentation documentation
+        documentation documentation
+        documentation documentation
+        """
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return (self.A)
+
+    def cost(self, Y, A):
+        """
+        documentation documentation
+        documentation documentation
+        documentation documentation
+        """
+        m = Y.shape[1]
+        cost = -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+        return (cost)
+
+    def evaluate(self, X, Y):
+        """
+        documentation documentation
+        documentation documentation
+        documentation documentation
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return (prediction, cost)
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """
+        documentation documentation
+        documentation documentation
+        documentation documentation
+        """
+        m = Y.shape[1]
+        dz = A - Y
+        db = np.sum(dz) / m
+        dw = np.matmul(X, dz.T) / m
+        self.__W = self.W - (alpha * dw).T
+        self.__b = self.b - alpha * db
+        return (self.W, self.b)
