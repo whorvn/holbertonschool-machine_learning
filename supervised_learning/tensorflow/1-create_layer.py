c8b208f6876b1d0ab5b65e41ec517efa07700db8
ravan <itzreone@gmail.com>
2025-01-11 18:14:39 +0400

    task 1

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,10 +7,12 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """create a layer for tensorflow"""
-    if activation == activation:
-        print("Tanh")
-        print("Relu")
-        print("Sigmoid")
+    if activation == tf.nn.tanh:
+        return("Tanh")
+    elif activation == tf.nn.relu:
+        return("Relu")
+    elif activation == tf.nn.sigmoid:
+        return("Sigmoid")
     init = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_AVG")
     layer = tf.layers.Dense(n,
