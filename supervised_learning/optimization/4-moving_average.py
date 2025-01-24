736e9f694e14871e0968f33879ff21fa2d632bd3
ravan <itzreone@gmail.com>
2025-01-24 14:44:17 +0400

    task 4 moving average

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module for creating mini-batches"""
+
+
+import numpy as np
+
+
+def moving_average(data, beta):
+    """
+    Calculates weighted moving average of a data set
+    Args:
+        data: list of data to calculate the moving average of
+        beta: weight used for the moving average
+    Returns:
+        list containing the moving averages of data
+    """
+    moving_averages = []
+    prev_avg = 0
+    for i in range(len(data)):
+        moving_avg = beta * prev_avg + (1 - beta) * data[i]
+        moving_averages.append(moving_avg)
+        prev_avg = moving_avg
+    return moving_averages
