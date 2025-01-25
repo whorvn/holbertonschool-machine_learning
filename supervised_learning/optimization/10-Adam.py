2f92d74c89e16d02b031861060c32358f5d73df0
ravan <itzreone@gmail.com>
2025-01-25 17:37:23 +0400

    adam opt

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module for creating mini-batches"""
+
+
+import tenserflow as tf 
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """Creates the training operation for a neural network in tensorflow using
+    the Adam optimization algorithm.
+    Args:
+        alpha: the learning rate.
+        beta1: the weight used for the first moment.
+        beta2: the weight used for the second moment.
+        epsilon: a small number to avoid division by zero.
+    Returns:
+        The Adam optimization operation.
+    """
+        optimizer = tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+    )
+    return optimizer
