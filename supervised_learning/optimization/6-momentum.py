2abf94650f35aae472627c5be512daf9ea2d1b44
ravan <itzreone@gmail.com>
2025-01-25 16:44:35 +0400

    optimization with tf

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for creating mini-batches"""
+
+
+import numpy as np
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """Creates a momentum operation in tensorflow using gradient descent"""
+    return tf.train.MomentumOptimizer(alpha, beta1)
