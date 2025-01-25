4e476682d5195e1181c71c44c05a9b3a74a9faf3
ravan <itzreone@gmail.com>
2025-01-25 18:10:17 +0400

    task 14 upd

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -18,17 +18,15 @@ def create_batch_norm_layer(prev, n, activation):
     Returns:
         tf.Tensor: The activated output of the layer.
     """
-    weights_initializer = tf.contrib.layers.variance_scaling_initializer(
-        mode="FAN_AVG")
-    layer = tf.layers.Dense(
-        n,
-        activation=activation,
-        name="layer",
-        kernal_initializer=weights_initializer)
-    x = layer[prev]
-    gamma = tf.Variable(tf.constant(
-        1, shape=(1, n), trainable=True, name="gamma"))
-    beta = tf.Variable(tf.constant(
-        0, shape=(1, n), trainable=True, name="gamma"))
-    Z = tf.nn.batch_normalization(x, mean, variance, beta, gamma, 1e-8)
-    return Z
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, kernel_initializer=init)
+
+    z = layer(prev)
+
+    mt, vt = tf.nn.moments(z, [0])
+    beta = tf.Variable(tf.zeros([z.get_shape()[-1]]))
+    gamma = tf.Variable(tf.ones([z.get_shape()[-1]]))
+    zt = tf.nn.batch_normalization(z, mt, vt, beta, gamma, 1e-8)
+    y_pred = activation(zt)
+
+    return y_pred
