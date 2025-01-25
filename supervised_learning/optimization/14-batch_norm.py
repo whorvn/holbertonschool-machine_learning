d3c353a8699f74f872d2c090b7a7221185b17c8a
ravan <itzreone@gmail.com>
2025-01-25 18:13:51 +0400

    task 14 upd

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -18,23 +18,15 @@ def create_batch_norm_layer(prev, n, activation):
     Returns:
         tf.Tensor: The activated output of the layer.
     """
-    # Initialize layer with variance scaling
-    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.keras.layers.Dense(units=n, kernel_initializer=init)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, kernel_initializer=init)
 
-    # Get layer output
     z = layer(prev)
 
-    # Calculate mean and variance
-    mean, variance = tf.nn.moments(z, axes=[0])
+    mt, vt = tf.nn.moments(z, [0])
+    beta = tf.Variable(tf.zeros([z.get_shape()[-1]]))
+    gamma = tf.Variable(tf.ones([z.get_shape()[-1]]))
+    zt = tf.nn.batch_normalization(z, mt, vt, beta, gamma, 1e-8)
+    y_pred = activation(zt)
 
-    # Create trainable parameters
-    gamma = tf.Variable(tf.ones([n]), trainable=True)
-    beta = tf.Variable(tf.zeros([n]), trainable=True)
-
-    # Apply batch normalization
-    z_norm = tf.nn.batch_normalization(
-        z, mean, variance, beta, gamma, epsilon=1e-7)
-
-    # Apply activation function
-    return activation(z_norm)
+    return y_pred
