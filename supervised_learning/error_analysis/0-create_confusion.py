eaaf40a511b0a737d41a4497a59030daae84fb40
ravan <itzreone@gmail.com>
2025-01-29 15:27:25 +0400

    error analysis

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+documentation documentation documentation
+"""
+
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    documentation documentation documentation
+    """
+    return np.matmul(labels.T, logits)
