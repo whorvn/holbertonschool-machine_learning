aedc99f0503a183e5ded9b42b9d8cd9e51bdf838
whorvn <itzreone@gmail.com>
2025-02-06 20:46:08 +0400

    grayscale

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""documentation documentation
+documentation documentation"""
+
+
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """documentation documentation
+    documentation documentation"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+    output = np.zeros((m, output_h, output_w))
+    for i in range(output_h):
+        for j in range(output_w):
+            output[:, i, j] = (images[:, i: i + kh, j: j + kw] * kernel
+                               ).sum(axis=(1, 2))
+    return output
