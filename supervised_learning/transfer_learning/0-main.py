b62f5861d588b0973ca5d4ec3ea6c37a232775d6
whorvn <itzreone@gmail.com>
2025-03-04 15:06:49 +0400

    transfer learning

--- supervised_learning/transfer_learning/0-main.py
+++ supervised_learning/transfer_learning/0-main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 from tensorflow import keras as K
-preprocess_data = __import__('0-transfer').preprocess_data
+from tensorflow import keras as K
+import tensorflow  as tf
+from tensorflow.python.keras import backend
+preprocess_data = __import__('0-transfer').resize_and_preprocess
 
 # to fix issue with saving keras applications
-K.learning_phase = K.backend.learning_phase 
+K.learning_phase = backend.symbolic_learning_phase()
 
 _, (X, Y) = K.datasets.cifar10.load_data()
 X_p, Y_p = preprocess_data(X, Y)
