fa645174d77094e9f39c50b9017c37d40635f056
whorvn <itzreone@gmail.com>
2025-03-06 14:17:47 +0400

    pycode

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -10,7 +10,7 @@ class Yolo:
     """
     This class implements the Yolo v3 algorithm to perform object detection.
     """
-    
+
     
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
